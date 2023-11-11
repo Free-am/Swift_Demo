82ec1e32f454a22d6f48e35f7c6131fd409c78b9
18610825000@126.com <Renchuan21>
2023-11-11 14:22:34 +0800

    Study 2
    函数、闭包

--- Swift/test.playground/Contents.swift
+++ Swift/test.playground/Contents.swift
@@ -131,3 +131,121 @@ import Foundation
 //    p += i
 //}
 //print(p)
+
+
+//// 函数和闭包
+//func greet(_ person: String, on day: String) -> String {
+//    let word = "Helo \(person), today is \(day)."
+//    print(word)
+//    return word
+//}
+//greet("Bob", on: "Monday")
+
+////返回多个值(元组)
+//func calculate(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+//    var min = scores[0]
+//    var max = scores[0]
+//    var sum = 0
+//    
+//    for score in scores {
+//        if score > max {
+//            max = score
+//        } else if score < min {
+//            min = score
+//        }
+//        sum += score
+//    }
+//    
+//    return (min, max, sum)
+//}
+//
+//let statics = calculate(scores: [5,3,100,3,9])
+//print(statics.sum)
+//print(statics.2)
+
+////嵌套函数
+//func returnFifteen() -> Int {
+//    var y = 10
+//    func add() {
+//        y += 5
+//    }
+//    add()
+//    return y
+//}
+//let x = returnFifteen()
+//print(x)
+
+//func method1() -> ((Int) -> Int) {
+//    func method2(number: Int) -> Int {
+//        return number + 1
+//    }
+//    return method2
+//}
+//var num = method1()
+//num(7)
+
+//func method3(num: [Int], condition: (Int) -> Bool) -> Bool {
+//    for n in num {
+//        if condition(n) {
+//            return true
+//        }
+//    }
+//    return false
+//}
+var arr = [20,19,7,12]
+//func numTest(num: Int) -> Bool {
+//    return num > 19
+//}
+//var state = method3(num: arr, condition: numTest(num:))
+//print(state)
+//
+//
+//var arr2 = arr.map({ (number: Int) -> String in
+//    return "\(number)"
+//})
+//arr = arr.map({$0 + 10})
+//print(arr2)
+//
+//let arr3 = arr.map({
+//    if $0 % 2 != 0 {
+//        return 0
+//    }
+//    return $0
+//})
+//print(arr3)
+//
+//let arr4 = arr.map({ $0*3 })
+//print(arr4)
+
+//var arr5: [[Int?]] = [[6,nil,18,33],[11,23,nil,4]]
+//var arr6 = arr5.map({$0.flatMap({$0})})
+//print(arr6)
+
+//var arr5: [Int?] = [6,nil,18,33]
+//var arr6 = arr5.compactMap{$0}
+//print(arr6)
+
+//let numbers2: [[Int?]] = [[1 ,2, 3],[4 , 5 ,6, nil]]
+//let resultFlatmap2 = numbers2.flatMap({$0.compactMap({$0})})
+//print(resultFlatmap2)//[3, 4, 5, 6, 7, 8]
+
+//let sortedArr = arr.sorted{$0 < $1}
+//print(sortedArr)
+
+
+////对象和类
+class Shape {
+    init(num: Int = 0) {
+        self.num = num
+        print("init")
+    }
+    var num = 0
+    func method() -> String {
+        return "\(num + 8)"
+    }
+}
+
+var shape = Shape()
+let a = shape.method()
+print(a)
+print(shape.num)
